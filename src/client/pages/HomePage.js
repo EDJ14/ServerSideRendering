c18aca3f99bcf41ad1650c218c61fb2cfca1e7f6
Eddie Jacobs <edjacobs@ucdavis.edu>
2019-05-06 13:43:21 -0700

    first commit

---
+++ src/client/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="center-align">
+      <h3>Welcome</h3>
+      <p>Check out dese awesome feters</p>
+    </div>
+  );
+};
+
+export default {
+  component: Home
+};
